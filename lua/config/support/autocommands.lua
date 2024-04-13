415ab090934b1f0dea1d78afd2b1b6497941a35e
Maks Titov <mstitovforgit@gmail.com>
2024-04-13 21:27:59 +0300

    refactoring autocomands.lua

--- lua/config/support/autocommands.lua
+++ lua/config/support/autocommands.lua
@@ -2,7 +2,8 @@ local api = vim.api
 
 api.nvim_create_autocmd("InsertEnter", { command = "set norelativenumber", pattern = "*" })
 api.nvim_create_autocmd("TermOpen", { command = "startinsert", pattern = "*" })
-api.nvim_create_autocmd("TermOpen", {
+api.nvim_create_autocmd("TermOpen", 
+{
     callback = function()
         -- disable line numbers
         vim.opt_local.number = false
@@ -12,19 +13,16 @@ api.nvim_create_autocmd("TermOpen", {
     end,
     pattern = "*"
 })
-api.nvim_create_autocmd(
-  {"TextChangedI", "TextChangedP"},
-  {
+api.nvim_create_autocmd({"TextChangedI", "TextChangedP"},
+{
     callback = function()
-      local line = api.nvim_get_current_line()
-      local cursor = api.nvim_win_get_cursor(0)[2]
+        local line = api.nvim_get_current_line()
+        local cursor = api.nvim_win_get_cursor(0)[2]
 
-      local current = string.sub(line, cursor, cursor + 1)
-      if after_line == "" or current == "#" then
-          require "cmp".complete()
-      end
-  end,
-  pattern = "*"
+        local current = string.sub(line, cursor, cursor + 1)
+        if after_line == "" or current == "#" then require "cmp".complete() end
+    end,
+    pattern = "*"
 })
 
 vim.diagnostic.config { virtual_text = false }
