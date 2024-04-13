fdb9e906abf657e15300680cc93126353a7ff517
Maks Titov <mstitovforgit@gmail.com>
2024-04-13 16:43:46 +0300

    nvim dap

--- lua/config/dap/cpp.lua
+++ lua/config/dap/cpp.lua
@@ -32,26 +32,6 @@ function M.setup()
                     ignoreFailures = false 
                 }
             }
-        },
-        {
-            name = 'Attach to gdbserver :1234',
-            type = 'cppdbg',
-            request = 'launch',
-            MIMode = 'gdb',
-            miDebuggerServerAddress = 'localhost:1234',
-            miDebuggerPath = '/usr/bin/gdb',
-            cwd = '${workspaceFolder}',
-            program = function()
-                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-            end,
-            setupCommands = 
-            {  
-                { 
-                    text = '-enable-pretty-printing',
-                    description =  'enable pretty printing',
-                    ignoreFailures = false 
-                }
-            }
         }
     }
 
