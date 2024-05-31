d664a2e07c779d01a9f70b8caef72b28dc93432f
Maks Titov <mstitovforgit@gmail.com>
2024-05-31 18:16:06 +0300

    refactoring dashboard

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -6,7 +6,8 @@ require "dashboard".setup
     change_to_vcs_root = false,
     preview = 
     {
-        command = "chafa --stretch -C on -c 240 -p on --polite on --color-space rgb -w 9 --dither bayer --dither-grain 4 --dither-intensity 1.5", -- preview command -C on -p on --polite on --threads 2  --stretch
+        command = "chafa --stretch -C on -c 240 -p on --polite on --color-space rgb -w 9 --dither bayer --dither-grain 2 --dither-intensity 1.0 --threads 2", 
+        -- preview command -C on -p on --polite on --threads 2  --stretch
         file_path = vim.fn.stdpath "config" .. "/static/nge.gif",     -- preview file path
         file_height = 36,  -- preview file height
         file_width = 120,    -- preview file width
@@ -16,30 +17,15 @@ require "dashboard".setup
         shortcut = 
         {
             { desc = "Recently latest session", group = "@property", action = "SessionLoad", key = "SPC u" },
-            {
-                desc = "Browse Files",
-                group = "DiagnosticHint",
-                action = "Telescope file_browser",
-                key = "SPC n",
-            },
-            {
-                desc = "Find File",
-                group = "Label",
-                action = "Telescope find_files",
-                key = "SPC f",
-            },
-            {
-                desc = "Configure Neovim",
-                group = "Number",
-                action = "edit ~/.config/nvim/init.lua",
-                key = "SPC v",
-            },
+            { desc = "Browse Files", group = "DiagnosticHint", action = "Telescope file_browser", key = "SPC n" },
+            { desc = "Find File", group = "Label", action = "Telescope find_files", key = "SPC f" },
+            { desc = "Configure Neovim", group = "Number", action = "edit ~/.config/nvim/init.lua", key = "SPC v" },
         },
         hide = { tabline = false },
         packages = { enable = true }, -- show how many plugins neovim loaded
         -- limit how many projects list, action when you press key or enter it will run this action.
         -- action can be a functino type, e.g.
-        -- action = func(path) vim.cmd('Telescope find_files cwd=' .. path) end
-        footer = { "NEOVIM STABLE v0.10.0" }, -- footer
+        -- action = func(path) vim.cmd("Telescope find_files cwd=" .. path) end
+        footer = { "NEOVIM STABLE v0.10.0" }
     }
 }
