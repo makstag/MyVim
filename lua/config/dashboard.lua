592b380b500e92549ec145fb9f3adf9612b93343
Maks Titov <mstitovforgit@gmail.com>
2024-07-15 19:36:54 +0300

    change keymap dashboard

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -16,7 +16,7 @@ require "dashboard".setup
     {
         shortcut = 
         {
-            { desc = "SSH Connect", group = "@property", action = "RemoteSSHFSConnect user@192.168.125.167", key = "u" },
+            { desc = "SSH Connect", group = "@property", action = "RemoteSSHFSConnect", key = "u" },
             { desc = "SSH Find File", group = "DiagnosticHint", action = "RemoteSSHFSFindFiles", key = "n" },
             { desc = "Find File", group = "Label", action = "Telescope find_files", key = "f" },
             { desc = "Configure Neovim", group = "Number", action = "edit ~/.config/nvim/init.lua", key = "v" },
