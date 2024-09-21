8c64572554bc1107e44e38f0a2b07b2303f926ca
Maks Titov <mstitovforgit@gmail.com>
2024-09-21 15:28:17 +0300

    add new autocommands

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -16,3 +16,12 @@ api.nvim_create_autocmd("QuickFixCmdPost", {
 		vim.cmd([[Trouble qflist open]])
 	end
 })
+api.nvim_create_autocmd("BufWritePre", {
+	group = vim.api.nvim_create_augroup("better_backup", { clear = true }),
+	callback = function(event)
+		local file = vim.uv.fs_realpath(event.match) or event.match
+		local backup = vim.fn.fnamemodify(file, ":p:~:h")
+		backup = backup:gsub("[/\\]", "%%")
+		vim.go.backupext = backup
+	end
+})
