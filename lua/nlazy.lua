6c37d9d3fc67aff74191965961aa59df361ebb88
makstag <MSTitovForGit@gmail.com>
2024-10-06 11:18:19 +0300

    Update nlazy.lua

--- lua/nlazy.lua
+++ lua/nlazy.lua
@@ -21,9 +21,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 require("lazy").setup({
 	colorscheme = "retrowave",
 	spec = { { import = "plugins" } },
