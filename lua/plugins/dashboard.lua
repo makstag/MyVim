5321176172781f1da95c405fccecf5761a8f25ac
makstag <MSTitovForGit@gmail.com>
2025-03-04 10:26:10 +0300

    Update dashboard.lua

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -43,7 +43,7 @@ return
 				-- limit how many projects list, action when you press key or enter it will run this action.
 				-- action can be a functino type, e.g.
 				-- action = func(path) vim.cmd("Telescope find_files cwd=" .. path) end
-				footer = { "🚀🚀🚀" }
+				-- footer = { "🚀🚀🚀" }
 			}
 		})
 	end
